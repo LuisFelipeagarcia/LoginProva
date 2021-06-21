988bb2e0ca8167fedeb563e7af52cdf253d8996e
LuisFelipeAGarcia <luisfelipeagarcia@gmail.com>
2021-06-21 14:49:07 -0300

    prova

---
+++ Login/Login/Data/IESContext.cs
@@ -0,0 +1,18 @@
+﻿using Login.Models.Infra;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Login.Data
+{
+    public class IESContext : IdentityDbContext<UsuarioDaAplicacao>
+    {
+        public IESContext(DbContextOptions<IESContext> options) : base(options)
+        {
+            
+        }
+    }
+}
---
+++ Login/Login/Data/IESDbInitializer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Login.Data
+{
+    public class IESDbInitializer
+    {
+        public static void Initialize(IESContext context)
+        {
+
+            context.Database.EnsureCreated();
+
+            context.SaveChanges();
+        }
+    }
+}
